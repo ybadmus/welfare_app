5e6354a6eeec35a53020aefa1795c2b883d36edf
ybadmus <yusif.badmus@gmail.com>
2021-04-22 02:55:29 +0800

    add scope for welfares that are not grouped

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,8 @@ class Group < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :icon, presence: true
 
+  scope :not_grouped, -> { left_outer_joins(:categories).where('categories.group_id' => nil) }
+
   belongs_to :user
   has_many :categories
   has_many :welfares, through: :categories
