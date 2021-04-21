ac854f673a5346e6dabe78861d3d548f872b0949
ybadmus <yusif.badmus@gmail.com>
2021-04-22 05:07:02 +0800

    add updates to migration file

--- db/migrate/20210421205809_create_groups.rb
+++ db/migrate/20210421205809_create_groups.rb
@@ -3,8 +3,7 @@ class CreateGroups < ActiveRecord::Migration[6.1]
     create_table :groups do |t|
       t.string :name
       t.string :icon
-      t.string :user_id
-      t.string :integer
+      t.integer :user_id
 
       t.timestamps
     end
--- db/migrate/20210421210042_create_welfares.rb
+++ db/migrate/20210421210042_create_welfares.rb
@@ -3,8 +3,7 @@ class CreateWelfares < ActiveRecord::Migration[6.1]
     create_table :welfares do |t|
       t.integer :authorId
       t.string :name
-      t.string :amount
-      t.string :integer
+      t.integer :amount
 
       t.timestamps
     end
