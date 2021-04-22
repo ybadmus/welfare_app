bfc51262c1a6d2727255222093498d32990e5989
ybadmus <yusif.badmus@gmail.com>
2021-04-22 16:14:22 +0800

    update route file with controllers path

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :welfares, only: [:index, :new, :create]
+  resources :groups, only: [:index, :new, :show, :create]
+  resources :users, only: [:new, :create]
+  
+  root 'sessions#index'
 end
