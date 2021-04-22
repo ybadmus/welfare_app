f9e7c82d2d35a4150a2e749b8c1209900daf7a32
ybadmus <yusif.badmus@gmail.com>
2021-04-23 05:29:12 +0800

    include remarks to welfare table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_210228) do
+ActiveRecord::Schema.define(version: 2021_04_22_205654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2021_04_21_210228) do
     t.integer "amount"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "remark"
   end
 
 end
