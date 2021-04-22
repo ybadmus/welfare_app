1a45a51c5250b945a612eca58999964d7b033d8b
ybadmus <yusif.badmus@gmail.com>
2021-04-23 03:47:51 +0800

    add welfare helper for welfare views

---
+++ app/helpers/welfare_helper.rb
@@ -0,0 +1,29 @@
+module WelfareHelper
+  def nav_title
+    if params[:external]
+      raw("<h1 class='nav-title'>External welfares</h1>")
+    else
+      raw("<h1 class='nav-title'>Welfares</h1>")
+    end
+  end
+
+  def total_amount(welfares)
+    raw("<p class='main-tracker'>
+      Total amount collected: <strong>#{welfares.total_amount}</strong>
+      </p>")
+  end
+
+  def welfare_icon(welfare)
+    group = welfare.groups.first
+
+    if group.nil?
+      #image_tag(exercise.user.icon, class: 'item-icon')
+    else
+      link_to image_tag(group.Icon), group_path(group.id), class: 'item-icon'
+    end
+  end
+
+  def welfare_amount(welfare)
+    raw("<strong> #{welfare.Amount.nil? ? '' : "GHS #{welfare.Amount}"} </strong>")
+  end
+end
