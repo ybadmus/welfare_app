17174e514d6ede417e9c04599ddc869573270ef6
ybadmus <yusif.badmus@gmail.com>
2021-04-27 00:20:57 +0100

    add tests for welfares controller

---
+++ spec/routing/welfares_routing_spec.rb
@@ -0,0 +1,18 @@
+  
+require 'rails_helper'
+
+RSpec.describe WelfaresController, type: :routing do
+  describe 'routing' do
+    it 'routes to #index' do
+      expect(get: '/welfares').to route_to('welfares#index')
+    end
+
+    it 'routes to #new' do
+      expect(get: '/welfares/new').to route_to('welfares#new')
+    end
+
+    it 'routes to #create' do
+      expect(post: '/welfares').to route_to('welfares#create')
+    end
+  end
+end
