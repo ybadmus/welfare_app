9a2df36ebb6a371f89b9a19867ab08b240c3ce3e
ybadmus <yusif.badmus@gmail.com>
2021-04-26 12:55:43 +0100

    add contributor feature complete

--- app/controllers/contributors_controller.rb
+++ app/controllers/contributors_controller.rb
@@ -2,27 +2,31 @@ class ContributorsController < ApplicationController
   before_action :require_login
 
   def index
-    @welfare = Contributor.all
+    @contributors = Contributor.all
   end
 
   def new
-    @welfare = Contributor.new
+    @contributor = Contributor.new
+    @icons = [
+      'https://www.flaticon.com/svg/static/icons/svg/3891/3891991.svg',
+      'https://www.flaticon.com/svg/static/icons/svg/3135/3135789.svg'
+    ]
   end
 
   def create
     @contributor = Contributor.create(contributor_params)
 
     if @contributor.save
-      redirect_to contributor_path, notice: 'New welfare contributor created.'
+      redirect_to contributors_path, notice: 'New welfare contributor created.'
     else
       flash.now[:alert] = @contributor.errors.full_messages.first
-      render :new
+      render :index
     end
   end
 
   private
 
   def contributor_params
-    params.require(:welfare).permit(:name, :phonenumber, :contributor_no)
+    params.require(:contributor).permit(:name, :phonenumber, :contributor_no)
   end
 end
