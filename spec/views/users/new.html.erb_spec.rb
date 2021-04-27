33a42c68f88e57f08b5c8968647b2a299265fb1f
ybadmus <yusif.badmus@gmail.com>
2021-04-27 02:37:51 +0100

    add tests for new.html file

---
+++ spec/views/users/new.html.erb_spec.rb
@@ -0,0 +1,9 @@
+  
+require 'rails_helper'
+
+RSpec.describe 'users/new', type: :feature do
+  it 'renders new user form' do
+    visit new_user_url
+    expect(:username).to match(/username/)
+  end
+end
